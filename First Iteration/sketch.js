c6901e2146df5012a1611c481861dc04195885a0
Yu7kun <168157781+Yu7kun@users.noreply.github.com>
2024-05-30 15:52:44 +1000

    First Iteration
    Keyboard operation
    Press the 'R' key:
    Function: Create a new non-overlapping circle on the screen.
    (But it seems not implemented)
    
    Press the '1' key:
    Function: Set the color of the ripples to red.
    
    Press the '2' key:
    Function: Set the color of the ripples to green.
    
    Press the '3' key:
    Function: Set the color of the ripples to blue.
    
    Mouse operation
    Click the mouse:
    Function: Create multiple ripple effects at the center of the circle at the click position (if the click position is inside a circle).
    
    Drag the mouse:
    Function: During the dragging process, create ripples of random colors along the path of the mouse.

---
+++ First Iteration/sketch.js
@@ -0,0 +1,179 @@
+let numCircles = 30;
+let circles = [];
+let ripples = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < numCircles; i++) {
+    let circle = createNonOverlappingCircle();
+    if (circle != null) {
+      circles.push(circle);
+    }
+  }
+}
+
+function draw() {
+  background(220);
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].display();
+  }
+
+  for (let i = ripples.length - 1; i >= 0; i--) {
+    ripples[i].update();
+    ripples[i].display();
+    if (ripples[i].isFinished()) {
+      ripples.splice(i, 1); // Remove finished ripples
+    }
+  }
+}
+
+function createNonOverlappingCircle() {
+  let newCircle = null;
+
+  let diameter = random(150, 200);
+  let circleColor = color(random(255), random(255), random(255));
+  let numSmallCircles = int(random(30, 50));
+  let smallCircleColor = color(random(255), random(255), random(255));
+  let numLayers = 5;
+  let tempCircle = new Circle(random(width), random(height), diameter, circleColor, numSmallCircles, smallCircleColor, numLayers);
+
+  let maxTries = 1000;
+  for (let i = 0; i < maxTries; i++) {
+    if (checkOverlap(tempCircle)) {
+      tempCircle.x = random(width);
+      tempCircle.y = random(height);
+    } else {
+      newCircle = tempCircle;
+      break;
+    }
+  }
+
+  return newCircle;
+}
+
+function checkOverlap(newCircle) {
+  for (let i = 0; i < circles.length; i++) {
+    let d = dist(newCircle.x, newCircle.y, circles[i].x, circles[i].y);
+    if (d < (newCircle.diameter / 2 + circles[i].diameter / 2)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+class Circle {
+  constructor(x, y, diameter, circleColor, numSmallCircles, smallCircleColor, numLayers) {
+    this.x = x;
+    this.y = y;
+    this.diameter = diameter;
+    this.circleColor = circleColor;
+    this.numSmallCircles = numSmallCircles;
+    this.smallCircleColor = smallCircleColor;
+    this.numLayers = numLayers;
+  }
+
+  display() {
+    fill(this.circleColor);
+    noStroke();
+    ellipse(this.x, this.y, this.diameter);
+
+    for (let i = 0; i < 8; i++) {
+      stroke(0);
+      ellipse(this.x, this.y, this.diameter * 0.5 - (i * 10));
+    }
+
+    for (let l = 1; l <= this.numLayers; l++) {
+      let layerRadius = this.diameter / 2 * (1 - l / (this.numLayers + 5));
+      for (let i = 0; i < this.numSmallCircles; i++) {
+        let angle = (TWO_PI / this.numSmallCircles * i);
+        let sx = this.x + cos(angle) * layerRadius;
+        let sy = this.y + sin(angle) * layerRadius;
+        fill(this.smallCircleColor);
+        ellipse(sx, sy, 10);
+      }
+    }
+  }
+
+  interactWithRipple(ripple) {
+    let d = dist(this.x, this.y, ripple.x, ripple.y);
+    if (d < ripple.diameter / 2) {
+      this.circleColor = ripple.color; // Change color when ripple interacts
+    }
+  }
+}
+
+class Ripple {
+  constructor(x, y, maxDiameter, speed, color) {
+    this.x = x;
+    this.y = y;
+    this.diameter = 0;
+    this.maxDiameter = maxDiameter;
+    this.speed = speed;
+    this.alpha = 255;
+    this.color = color;
+  }
+
+  update() {
+    this.diameter += this.speed;
+    this.alpha -= 2;
+  }
+
+  display() {
+    noFill();
+    stroke(this.color.levels[0], this.color.levels[1], this.color.levels[2], this.alpha);
+    ellipse(this.x, this.y, this.diameter);
+  }
+
+  isFinished() {
+    return this.alpha <= 0;
+  }
+}
+
+let rippleColor = color(0);
+
+// Handle keyboard input
+function keyPressed() {
+  if (key === 'R') {
+    // Press 'R' to create a new non-overlapping circle
+    let circle = createNonOverlappingCircle();
+    if (circle != null) {
+      circles.push(circle);
+    }
+  } else if (key === '1') {
+    // Press '1' to set ripple color to red
+    rippleColor = color(255, 0, 0); // Red
+  } else if (key === '2') {
+    // Press '2' to set ripple color to green
+    rippleColor = color(0, 255, 0); // Green
+  } else if (key === '3') {
+    // Press '3' to set ripple color to blue
+    rippleColor = color(0, 0, 255); // Blue
+  }
+}
+
+// Handle mouse press input
+function mousePressed() {
+  for (let i = 0; i < circles.length; i++) {
+    let d = dist(mouseX, mouseY, circles[i].x, circles[i].y);
+    if (d < circles[i].diameter / 2) {
+      // Create multiple ripples when clicking inside a circle
+      let colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255)];
+      for (let j = 0; j < 3; j++) {
+        let ripple = new Ripple(circles[i].x, circles[i].y, circles[i].diameter * 2, random(1, 5), colors[j]);
+        ripples.push(ripple);
+      }
+    }
+  }
+}
+
+// Handle mouse drag input
+function mouseDragged() {
+  // Create a ripple at the mouse position with random color while dragging
+  let ripple = new Ripple(mouseX, mouseY, 200, 2, color(random(255), random(255), random(255)));
+  ripples.push(ripple);
+}
+
+// Handle window resize
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
